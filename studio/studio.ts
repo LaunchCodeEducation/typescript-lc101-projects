1bfc1e388915927f0a1cdf06657f496c1f4a8460
Jim Flores <jimflores@Jims-MacBook-Air.local>
2019-08-19 14:44:53 -0500

    First draft of starter file for TypeScript studio.

---
+++ studio/studio.ts
@@ -0,0 +1,6 @@
+// Instructions are published in the online book. The URL is:
+// https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/studio.html
+
+// TODO: 
+// * Code the Astronaut, Cargo, and Rocket classes in new files.
+// * Import the three classes into this file.

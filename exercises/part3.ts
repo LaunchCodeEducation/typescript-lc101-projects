22e971a2e0870f11c4e429013e10cb1d11821d2f
Jim Flores <jimflores@Jims-MacBook-Air.local>
2019-08-19 14:10:13 -0500

    Consistency update for part3.ts.

--- exercises/part3.ts
+++ exercises/part3.ts
@@ -1,13 +1,16 @@
+// URL for the instructions: 
+// https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
+
 let spacecraftName: string = "Space Shuttle";
 let speedMph: number = 17500;
 let kilometersToMars: number = 225000000;
 let kilometersToTheMoon: number = 384400;
 let milesPerKilometer: number = 0.621;
 
-// Part 3 - Code the getDaysToLocation function here:
+// Code the "getDaysToLocation" function here:
 
 
 
 
 
-// Call the function and print the output here:
+// Call the function and print the output:

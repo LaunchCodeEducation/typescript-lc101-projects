db32d23934ae2cea3ec1c9eff005487c15eda4e0
jimflores <jflores@launchcode.org>
2020-02-05 13:12:30 -0600

    Continued work to condense the exercises into 1 ts file.

--- exercises/SpaceLocation.ts
+++ exercises/SpaceLocation.ts
@@ -1,10 +1,1 @@
 // Paste in the provided code here:
-export class SpaceLocation {
-    kilometersAway: number;
-    name: string;
-
-    constructor(name: string, kilometersAway: number) {
-        this.name = name;
-        this.kilometersAway = kilometersAway;
-    }
-}
---
+++ exercises/parts1-5.ts
@@ -0,0 +1,42 @@
+// URL for the instructions: 
+// https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
+
+
+// Part 1: Add the 5 variables here
+
+
+
+// Part 2:
+
+
+// Code the output statement here (use a template literal):
+
+
+
+// Part 3:
+// Code the "getDaysToLocation" function here:
+
+
+// Call the function and print the outputs for the Mars trip and the moon trip:
+
+
+// Part 4:
+/* Move the variables spacecraftName, speedMph, and milesPerKilometer into the
+    Spacecraft class. Also move your getDaysToLocation function into the Spacecraft
+    class. */
+
+// Define your Spacecraft class here:
+
+
+// Create an instance of the class here:
+
+
+
+// Print two outputs - one for the trip to Mars and one for the trip to the moon.
+
+// Part 5: Add the required import statement BEFORE the part 1 concent.
+
+
+// Add the printDaysToLocation function to the Spacecraft class.
+
+// Paste in the code from step 6 here:

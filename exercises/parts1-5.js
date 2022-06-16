55135db184f0d519ddbc7d4d760be5a36158d822
AnnaK <julie_jia678@hotmail.com>
2022-06-16 14:38:11 -0400

    Finished Exercise

--- exercises/parts1-5.js
+++ exercises/parts1-5.js
@@ -1,55 +1,34 @@
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
-
-
 // Part 1: Declare (5) Variables With Type
-let kilometersToMars: number = 225000000;
-let kilometersToTheMoon: number = 384400;
-
+var kilometersToMars = 225000000;
+var kilometersToTheMoon = 384400;
 // Part 2: Print Days to Mars
-
-
-
 // Code an output statement here (use a template literal):
-
-
-
 // Part 3: Create a Function ("getDaysToLocation")
-
-
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
-
 // Part 4: Create a Spacecraft Class
-class Spacescraft {
-    milePerKilometer: number = 0.621;
-    name: string;
-    speedMph: number;
-
-    constructor(name: string, speedMph: number) {
+var Spacescraft = /** @class */ (function () {
+    function Spacescraft(name, speedMph) {
+        this.milePerKilometer = 0.621;
         this.name = name;
-        this.speedMph = speedMph;    
+        this.speedMph = speedMph;
     }
-
-    getDaysToLocation (kilometerAway: number): number {
-        let milesAway: number = kilometerAway * this.milePerKilometer;
-        let hoursToLocation: number = milesAway / this.speedMph;    
-        return hoursToLocation / 24; 
-    }
-}
-
+    Spacescraft.prototype.getDaysToLocation = function (kilometerAway) {
+        var milesAway = kilometerAway * this.milePerKilometer;
+        var hoursToLocation = milesAway / this.speedMph;
+        return hoursToLocation / 24;
+    };
+    return Spacescraft;
+}());
 // Create an instance of the class here:
-let spaceShuttle = new Spacescraft("Determination", 17500);
-
+var spaceShuttle = new Spacescraft("Determination", 17500);
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
-console.log(`${spaceShuttle.name} takes ${spaceShuttle.getDaysToLocation(kilometersToMars)} days to Mars.`);
-console.log(`${spaceShuttle.name} takes ${spaceShuttle.getDaysToLocation(kilometersToTheMoon)} days to Moon.`);
-
+console.log("".concat(spaceShuttle.name, " takes ").concat(spaceShuttle.getDaysToLocation(kilometersToMars), " days to Mars."));
+console.log("".concat(spaceShuttle.name, " takes ").concat(spaceShuttle.getDaysToLocation(kilometersToTheMoon), " days to Moon."));
 // Part 5: Export and Import the SpaceLocation Class
 // Add the required import statement BEFORE the part 1 concent.
-
-
 // Add the printDaysToLocation function to the Spacecraft class.
-
 // Paste in the code from step 6 here:
